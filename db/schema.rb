560cbfe761194be082daad4d8934d8b90986834e
beherit1977 <beherit1977@gmail.com>
2018-12-17 18:38:50 +0000

    add null

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_17_170728) do
+ActiveRecord::Schema.define(version: 2018_12_17_183048) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text", null: false
@@ -20,14 +20,14 @@ ActiveRecord::Schema.define(version: 2018_12_17_170728) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.integer "level"
-    t.string "title"
+    t.integer "level", null: false
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "text"
+    t.string "text", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

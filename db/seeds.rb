d5390a8be94263da339d5347e40f86d907def2f9
beherit1977 <beherit1977@gmail.com>
2018-12-18 15:38:58 +0000

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = Category.create(
+    [{ title: :language }, { level: 1} ])
+
+tests = Test.create(
+    [{ title: 'Java', level: 3 },
+     { title: 'Perl', level: 2 },
+     { title: 'Ruby', level: 1 },
+     { title: 'Python', level: 2},
+     { title: 'Javascript', level: 3 }]
+)
+
+questions = Question.create(
+    [{ text: 'Java question #1' },
+     { text: 'Perl question #1' },
+     { text: 'Ruby question #1' },
+     { text: 'Python question #1' },
+     { text: 'Javascript question #1' }]
+)
+
+Answer.create(
+    [{ text: 'Answer 1q1' },
+     { text: 'Answer 2q1' },
+     { text: 'Answer 1q2' },
+     { text: 'Answer 2q2' },
+     { text: 'Answer 1q3' },
+     { text: 'Answer 2q3' },
+     { text: 'Answer 1q4' },
+     { text: 'Answer 2q4' },
+     { text: 'Answer 1q5' },
+     { text: 'Answer 2q5' }]
+)
+
+users = User.create(
+    [{ name: :firstuser },
+     { name: :seconduser }]
+)
+

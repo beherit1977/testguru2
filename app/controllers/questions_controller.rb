65038c0b8824503d0a6a1492f93c2a61e8072842
beherit1977 <beherit1977@gmail.com>
2019-01-14 08:00:00 +0000

    add controllers and views

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,50 @@
+class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index create new]
+  before_action :find_question, only: %i[show]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    @questions = @test.questions
+  end
+
+  def show
+    render plain: "#{@question.body}"
+  end
+
+  def new
+    @question = @test.questions.new
+  end
+
+  def create
+    @question =  @test.questions.new(question_params)
+    if @question.save
+      redirect_to @question
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id]).destroy
+    render plain: 'Deleted!'
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question was not found'
+  end
+end

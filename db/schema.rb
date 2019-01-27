b5c7939231ae7d5913b7c933f0b1c5d8be7e8e63
beherit1977 <beherit1977@gmail.com>
2019-01-27 08:14:19 +0000

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_24_172325) do
+ActiveRecord::Schema.define(version: 2019_01_27_073606) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text", null: false

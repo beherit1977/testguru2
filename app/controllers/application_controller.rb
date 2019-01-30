89ec087d3db7815f5d1c2f1e004674fac52ea364
Serg <beherit1977@gmail.com>
2019-01-30 11:13:34 +0000

    refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  config.time_zone = 'Moscow'
   I18n.config.available_locales = :ru
   I18n.config.default_locale = :ru
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_27_083457) do
+ActiveRecord::Schema.define(version: 2019_01_30_110833) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text", null: false
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2019_01_27_083457) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

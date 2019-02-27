c420edf60fbab558827eee5647891a737d075cab
beherit1977 <beherit1977@gmail.com>
2019-02-27 11:17:35 +0000

    1

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_184541) do
     t.integer "test_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "current_question_id"
+    t.integer "current_question_id", index: true
     t.integer "correct_questions", default: 0
     t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
     t.index ["test_id"], name: "index_test_passages_on_test_id"

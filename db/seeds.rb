fe4e60e41b2f545eb76a70f8ddd28372005a638d
Serg <beherit1977@gmail.com>
2019-03-04 06:48:15 +0000

    seed correction

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@ TestPassage.delete_all
 Test.delete_all
 User.delete_all
 
-users = User.create! [{ first_name: 'a', last_name: 'b', email: 'a@a.a', password: '111111', confirmed_at: Time.now },
-                      { first_name: 'z', last_name: 'z', email: 'z@z.z', type: "Admin", password: '111111', confirmed_at: Time.now },
-                      { first_name: 's', last_name: 's', email: 'beherit1977@gmail.com', type: "Admin", password: '111111', confirmed_at: Time.now }]
+users = User.create! [{ first_name: 'Августин', last_name: 'Светлый', email: 'a@a.a', password: '111111', confirmed_at: Time.now },
+                      { first_name: 'Зураб', last_name: 'Церетелли', email: 'z@z.z', type: "Admin", password: '111111', confirmed_at: Time.now },
+                      { first_name: 'Сергей', last_name: 'Бурый', email: 'beherit1977@gmail.com', type: "Admin", password: '111111', confirmed_at: Time.now }]
 
 categories = Category.create! [{ title: 'Frontend', level: 1 }, { title: 'Backend', level: 2 }, { title: 'Full_stack', level: 2} ]
 
@@ -28,8 +28,8 @@ questions = Question.create! [{ text: 'Что такое HTML', test_id: tests[0
 answers = Answer.create! [{ text: 'HTML язык разметки', correct: true, question_id: questions[0].id},
                           { text: 'HTML язык програмирования', correct: false, question_id: questions[0].id },
                           { text: 'HTML язык аборигенов', correct: false, question_id: questions[1].id },
-                          { text: 'Ответ', correct: true, question_id: questions[3].id },
-                          { text: 'Ответ', correct: true, question_id: questions[4].id },
+                          { text: 'Еще один правильный ответ', correct: true, question_id: questions[3].id },
+                          { text: 'Еще один правильный ответ', correct: true, question_id: questions[4].id },
                           { text: 'PHP язык програмирования', correct: true, question_id: questions[1].id },
                           { text: 'PHP это еда', correct: false, question_id: questions[2].id },
                           { text: 'PHP это ад', correct: false, question_id: questions[2].id },
